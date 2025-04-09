c4a64e93b0fbda573ca55d28bb32ebfffadb232a
[UFPB] Jonathan Neves <jonathan.neves@dcx.ufpb.br>
2025-04-09 17:19:09 -0300

    Removendo os templates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <p>Home view</p>
+  </div>
 </template>
