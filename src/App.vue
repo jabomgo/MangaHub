4b3d123b3ed370521201c6742a43df48bf3ef12c
jonathan.neves <jonathan.neves@dcx.ufpb.br>
2025-04-14 16:36:43 -0300

    Função de pesquisar indopar a página de pesquisar com a query pesquisada

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,23 @@ import 'primeicons/primeicons.css'
 import { RouterView } from 'vue-router'
 import router from './router'
 import { ref } from 'vue'
-
+const showSearch = ref(false)
+const searchInput = ref('')
 const items = ref([
   {
     label: 'MangaHub',
     icon: 'pi pi-home',
-    command: () => pushTo('about'),
+    command: () => pushTo('home'),
   },
   {
     label: 'Favoritos',
     icon: 'pi pi-heart',
     command: () => pushTo('favoritos'),
+  },
+  {
+    label: 'Pesquisar',
+    icon: 'pi pi-search',
+    command: ($event) => toggleSearch($event),
   },
   {
     label: 'Conta',
@@ -33,6 +39,13 @@ const items = ref([
   },
 ])
 
+function toggleSearch(event) {
+  showSearch.value = !showSearch.value
+}
+
+function search(query) {
+  router.push({ name: 'pesquisando', query: { query } })
+}
 function pushTo(routerName) {
   router.push({ name: routerName })
 }
@@ -42,6 +55,14 @@ function pushTo(routerName) {
   <nav>
     <div class="main-bar">
       <Menubar :model="items" />
+      <InputText
+        ref="mainBarSearch"
+        v-if="showSearch"
+        @keyup.enter="search(searchInput)"
+        v-model="searchInput"
+        style="width: 100%"
+        placeholder="Pesquise um manga..."
+      />
     </div>
   </nav>
   <RouterView />
