d1f9836756a01dd28bc428a2f9868a7e1ed194f7
jonathan.neves <jonathan.neves@dcx.ufpb.br>
2025-04-14 15:33:13 -0300

    Ajuste no menu
    Consegue rotear para as respectivas páginas

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,50 @@
 <script setup>
 import 'primeicons/primeicons.css'
-import { RouterLink, RouterView } from 'vue-router'
-
-import { ref } from "vue";
+import { RouterView } from 'vue-router'
+import router from './router'
+import { ref } from 'vue'
 
 const items = ref([
   {
-      label: 'Home',
-      icon: 'pi pi-home'
+    label: 'MangaHub',
+    icon: 'pi pi-home',
+    command: () => pushTo('about'),
   },
   {
-      label: 'Projects',
-      icon: 'pi pi-search',
-      badge: 3,
-      items: [
-          {
-              label: 'Core',
-              icon: 'pi pi-bolt',
-              shortcut: '⌘+S'
-          },
-          {
-              label: 'Blocks',
-              icon: 'pi pi-server',
-              shortcut: '⌘+B'
-          },
-          {
-              separator: true
-          },
-          {
-              label: 'UI Kit',
-              icon: 'pi pi-pencil',
-              shortcut: '⌘+U'
-          }
-      ]
-  }
-]);
+    label: 'Favoritos',
+    icon: 'pi pi-heart',
+    command: () => pushTo('favoritos'),
+  },
+  {
+    label: 'Conta',
+    icon: 'pi pi-user',
+    items: [
+      {
+        label: 'Login',
+        icon: 'pi pi-key',
+        command: () => pushTo('login'),
+      },
+      {
+        label: 'Sair',
+        icon: 'pi pi-sign-out',
+        command: () => console.log('logout'),
+      },
+    ],
+  },
+])
+
+function pushTo(routerName) {
+  router.push({ name: routerName })
+}
 </script>
 
 <template>
+  <nav>
+    <div class="main-bar">
+      <Menubar :model="items" />
+    </div>
+  </nav>
   <RouterView />
-
-  <div class="card">
-      <Menubar :model="items">
-          <template #start>
-              <img src="../public/favicon.ico" alt="Logo" width="35" height="40" class="h-8">
-          </template>
-          <template #item="{ item, props, hasSubmenu, root }">
-              <a v-ripple class="flex items-center" v-bind="props.action">
-                  <span>{{ item.label }}</span>
-                  <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
-                  <span v-if="item.shortcut" class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1">{{ item.shortcut }}</span>
-                  <i v-if="hasSubmenu" :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
-              </a>
-          </template>
-          <template #end>
-              <div class="flex items-center gap-2">
-                  <InputText placeholder="Search" type="text" class="w-32 sm:w-auto" />
-                  <Avatar image="../public/favicon.ico" shape="circle" />
-              </div>
-          </template>
-      </Menubar>
-  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
