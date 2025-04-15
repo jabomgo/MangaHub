97a41cf6cf9f5817083b6a3d7bcbffc8c3b1394a
cidy <cidywallace12@gmail.com>
2025-04-15 20:24:49 -0300

    adicionando itens ao explorar da página inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import axiosMangaDex from '../axios'
 
+const titles = ref([])
+
+onMounted(async () => {
+  const mangaData = await axiosMangaDex.searchManga('')
+  const mangasExplorar = mangaData.data;
+
+  titles.value = mangasExplorar.map(manga => manga.attributes.title.en)
+})
 </script>
 
 <template>
   <div>
-    <p>Home view</p>
-
+    <p>Explorar</p>
+    <ul>
+      <li v-for="(title, index) in titles" :key="index">{{ title }}</li>
+    </ul>
   </div>
 
 </template>
