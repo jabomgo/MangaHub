b0afb38386e52fbdf2a46e0b322b9c2ae8d8462e
[UFPB] Jonathan Neves <jonathan.neves@dcx.ufpb.br>
2025-04-15 18:29:46 -0300

    [FORMATAÇÃO] Fortação prettier

--- src/axios.js
+++ src/axios.js
@@ -18,68 +18,72 @@ class axiosMangaDex {
   }
 
   // Requisisão que retorna mangas pesquisado pelo capítulo
-  async searchManga(title){
-    try{
+  async searchManga(title) {
+    try {
       const response = await this.apiMangaDex.get(`/manga?title=${title}`)
       return response.data
-    } catch(error){
-      console.error("Erro ao buscar manga:", error)
+    } catch (error) {
+      console.error('Erro ao buscar manga:', error)
       throw error
     }
   }
 
   // Requisição para pegar fileName do manga
   async getFileCover(coveId) {
-    try{
+    try {
       const response = await this.apiMangaDex.get(`/cover/${coveId}`)
       return response.data
-    } catch(error){
-      console.error("Erro ao obter coverID:", error)
+    } catch (error) {
+      console.error('Erro ao obter coverID:', error)
       throw error
     }
   }
 
   // Requisição para pegar imagem de capa do manga
   async getCoverArt(mangaId, fileName) {
-    try{
-      const response = await this.uploadMangaDex.get(`/covers/${mangaId}/${fileName}`, {responseType: 'blob'})
+    try {
+      const response = await this.uploadMangaDex.get(`/covers/${mangaId}/${fileName}`, {
+        responseType: 'blob',
+      })
       const coverUrl = URL.createObjectURL(response.data)
       return coverUrl
-    } catch(error){
-      console.error("Erro ao obter imagem da capa:", error)
+    } catch (error) {
+      console.error('Erro ao obter imagem da capa:', error)
       throw error
     }
   }
 
   // Requisição para pegar todos ids dos volumes e capítulos disponíveis. Obs: Não necessáriamente em Pt-br
   async getCapterVolume(mangaId) {
-    try{
+    try {
       const response = await this.apiMangaDex.get(`/manga/${mangaId}/aggregate`)
       return response.data
-    } catch(error){
-      console.error("Erro ao obter os volumes e capítulos do manga: ", error)
+    } catch (error) {
+      console.error('Erro ao obter os volumes e capítulos do manga: ', error)
       throw error
     }
   }
 
   // Requisição para pegar as URLs das páginas de capítulo escolhido
   async getCapterPage(capterId) {
-    try{
+    try {
       const response = await this.apiMangaDex.get(`/at-home/server/${capterId}`)
       return response.data
-    } catch(error) {
-      console.error("Erro ao obter dados das páginas: ", error)
+    } catch (error) {
+      console.error('Erro ao obter dados das páginas: ', error)
       throw error
     }
   }
 
   // Requisição para pegar imagem do capítulo do manga
   async getImagePage(pageId, hashId) {
-    try{
-      const response = await this.uploadMangaDex.get(`/data/${hashId}/${pageId}`, {responseType: 'blob'})
+    try {
+      const response = await this.uploadMangaDex.get(`/data/${hashId}/${pageId}`, {
+        responseType: 'blob',
+      })
       const pageData = URL.createObjectURL(response.data)
       return pageData
-    }catch(error) {
+    } catch (error) {
       console.error(error)
       throw error
     }
@@ -88,13 +92,15 @@ class axiosMangaDex {
   // Requisição para pegar os volumes e capitulos em uma língua especifica
   async getCapterVolumeForLangage(mangaId, idioma) {
     try {
-      const response = await this.apiMangaDex.get(`/manga/${mangaId}//feed?translatedLanguage[]=${idioma}`)
+      const response = await this.apiMangaDex.get(
+        `/manga/${mangaId}//feed?translatedLanguage[]=${idioma}`,
+      )
       return response.data
-    } catch(error) {
+    } catch (error) {
       console.error(error)
       throw error
     }
   }
 }
 
-export default new axiosMangaDex();
+export default new axiosMangaDex()
