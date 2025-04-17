ab35b1f1454e698191d6879f0c652dba1d2825fc
[UFPB] Jonathan Neves <jonathan.neves@dcx.ufpb.br>
2025-04-17 11:48:28 -0300

    Template estático de listagem de capitulos

--- src/views/CapitulosView.vue
+++ src/views/CapitulosView.vue
@@ -1,13 +1,54 @@
 <script setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 
+const router = useRoute();
+
+const mangaCapitulos = ref([1, 1, 1]);
+const mangaCover = ref(
+  "https://static.vecteezy.com/ti/fotos-gratis/t2/36324708-ai-gerado-cenario-do-uma-tigre-caminhando-dentro-a-floresta-foto.jpg",
+);
+const mangaId = ref("");
+const capituloFilter = ref(null)
+
+onMounted(async () => {
+  mangaId.value = router.query.manga;
+});
 </script>
 
 <template>
-  <div>
-    <p>Capitulos View</p>
+  <div class="capitulos-container grid w-full my-4">
+    <div class="manga-sobre col-12 md:col-4 lg:col-3">
+      <Card class="w-full h-full" style="overflow: hidden">
+        <template #header>
+          <div
+            class="flex align-items-center justify-content-center"
+            style="height: 300px; background-color: #f4f4f4"
+          >
+            <img alt="user header" :src="mangaCover" class="w-auto h-full object-contain" />
+          </div>
+        </template>
+        <template #title>Título manga</template>
+        <template #subtitle>Descrição</template>
+        <template #content>
+          <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+        </template>
+      </Card>
+    </div>
+
+    <div class="capitulos-list col-12 md:col-8 lg:col-9">
+      <div class="flex flex-column gap-3 w-full">
+        <InputNumber v-model="capituloFilter" showButtons prefix="Ir para capítulo " placeholder="Ir para capítulo..."/>
+
+        <Card v-for="(capitulo, index) in mangaCapitulos" :key="index" class="w-full">
+          <template #title>Capítulo {{ index + 1 }}</template>
+          <template #content>
+            <Button label="Ler capítulo" />
+          </template>
+        </Card>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
