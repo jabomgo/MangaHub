ae92289720442101d3e22633ec2f3ce5bd4b7ebc
jonathan.neves <jonathan.neves@dcx.ufpb.br>
2025-04-24 16:27:31 -0300

    Ajustes no loading para indicar que algo está acontecendo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,56 @@
+<template>
+  <div class="explorar-container">
+    <h1 class="title-container" v-if="!loading">Explorar</h1>
+
+    <div v-if="loading" class="skeleton-container">
+      <Skeleton v-for="n in numDefaultCards" :key="n" width="20rem" height="370px" class="skeleton-card" />
+    </div>
+
+    <div v-else class="card-list">
+      <Card
+        v-for="(item, index) in mangaInfo"
+        :key="index"
+        class="card-item"
+        @click="navigateToCapitulos(item.id)"
+      >
+        <template #content>
+          <div v-if="!item.imageLoad" class="image-placeholder">
+            <i class="pi pi-spin pi-spinner" style="font-size: 2rem; color: #fff;"></i>
+          </div>
+          <div v-else class="image-container">
+            <img v-if="item.Url" :src="item.Url" alt="cover_art" class="card-image" />
+          </div>
+        </template>
+        <template #footer>{{ item.title }}</template>
+      </Card>
+    </div>
+
+    <div class="button-container">
+      <Button
+        label="Ver mais"
+        icon="pi pi-plus"
+        :loading="loading"
+        loadingIcon="pi pi-spin pi-spinner"
+        @click="navigateToPesquisa"
+      />
+    </div>
+  </div>
+</template>
+
 <script setup>
 import { ref, onMounted } from "vue";
 import axiosMangaDex from "../axios";
 import { useRouter } from 'vue-router';
+import Skeleton from 'primevue/skeleton';
 
 const mangaInfo = ref([]);
 const coverFileNames = ref([]);
 const router = useRouter();
-const loading = ref(true)
+const loading = ref(true);
+const numDefaultCards = ref(5)
 
 const explorarMangas = async () => {
-  const mangaData = await axiosMangaDex.searchManga("", 4);
+  const mangaData = await axiosMangaDex.searchManga("", numDefaultCards.value);
   const mangasExplorar = mangaData.data;
 
   mangaInfo.value = mangasExplorar.map((manga) => ({
@@ -26,13 +67,12 @@ const explorarMangas = async () => {
 
   await fetchCoverFileNames();
   await fetchUrlImage();
-  loading.value = false
-}
+  loading.value = false;
+};
 
 const fetchCoverFileNames = async () => {
   for (let i = 0; i < mangaInfo.value.length; i++) {
     const manga = mangaInfo.value[i];
-
     if (manga.coverId) {
       const coverResponse = await axiosMangaDex.getFileCover(manga.coverId);
       coverFileNames.value.push({
@@ -46,67 +86,59 @@ const fetchUrlImage = async () => {
   for (let i = 0; i < coverFileNames.value.length; i++) {
     const cover = coverFileNames.value[i];
     const manga = mangaInfo.value[i];
-
     if (manga.coverId) {
       const Url = await axiosMangaDex.getCoverArt(manga.id, cover.fileName, 256);
       manga.Url = Url;
-      manga.imageLoad = true
+      manga.imageLoad = true;
     }
   }
 };
 
 function navigateToCapitulos(manga) {
-  router.push({name: 'capitulos', query: {manga}});
+  router.push({ name: 'capitulos', query: { manga } });
 }
 
 function navigateToPesquisa() {
-  router.push('/pesquisando');
+  loading.value = true;
+  setTimeout(() => {
+    router.push('/pesquisando');
+  }, 500);
 }
+
 onMounted(() => {
   explorarMangas();
 });
 </script>
 
-<template>
-  <div class="">
-    <h1 class="title-container">Explorar</h1>
-
-    <div class="card-list">
-      <Card
-        v-for="(item, index) in mangaInfo"
-        :key="index"
-        style="width: 20rem; box-sizing: content-box; margin: 0.5px; overflow: hidden; transition: transform 0.3s ease, box-shadow 0.3s ease;cursor: pointer;"
-        class="mb-3, card-item"
-        @click="navigateToCapitulos(item.id)"
-      >
-        <!-- Header com a imagem do manga -->
-        <template #content>
-          <div v-if="!item.imageLoad" class="image-placeholder">
-            <i class="pi pi-spin pi-spinner" style="font-size: 2rem; color: #fff;"></i>
-          </div>
-          <div v-else class="image-container">
-            <img v-if="item.Url" :src="item.Url" alt="cover_art" class="card-image" />
-          </div>
-        </template>
-
-        <template #footer>{{ item.title }}</template>
-      </Card>
-
-    </div>
-    <div v-if="!loading" class="button-container">
-      <Button label="Ver mais"
-      icon="pi pi-plus"
-      @click="navigateToPesquisa"/>
-    </div>
-  </div>
-
-</template>
-
 <style scoped>
+.explorar-container {
+  padding: 1rem;
+}
+
+.title-container {
+  padding-bottom: 0.5rem;
+  color: white;
+  display: flex;
+  justify-content: left;
+  font-size: 2rem;
+}
+
 .card-list {
   display: flex;
   flex-wrap: wrap;
-  gap: 0.3rem;
+  gap: 1rem;
+  justify-content: center;
+}
+
+.card-item {
+  width: 20rem;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  cursor: pointer;
+}
+
+.card-item:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 4px 10px rgba(196, 194, 194, 0.1);
 }
 
 .image-container {
@@ -114,11 +146,6 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
   height: 370px;
-}
-
-.card-item:hover {
-  transform: translateY(-5px);
-  box-shadow: 0 4px 10px rgba(196, 194, 194, 0.1);
 }
 
 .card-image {
@@ -130,10 +157,24 @@ onMounted(() => {
 
 .image-placeholder {
   width: 100%;
-  height: 100%;
+  height: 370px;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: #ccc;
+}
+
+.skeleton-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+  margin-top: 1rem;
+}
+
+.skeleton-card {
+  width: 20rem;
+  height: 370px;
 }
 
 .button-container {
@@ -141,12 +182,5 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   width: 100%;
-}
-
-.title-container {
-  padding-bottom: 0.5rem;
-  color: white;
-  display: flex;
-  justify-content: center;
 }
 </style>
