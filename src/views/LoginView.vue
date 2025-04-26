fbd1f963e07420c6bb8e19a4c3ad16414589479a
cidy <cidywallace12@gmail.com>
2025-04-26 12:45:41 -0300

    adicionando tela de login e json-serve

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,40 @@
 <script setup>
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 </script>
 
 <template>
-  <div>
-    <p>login View</p>
+  <div class="flex" style="justify-content: center; padding-top: 8rem;">
+    <!-- <p>login View</p> -->
+
+    <Card class="flex align-items-center w-25rem h-19rem surface-200">
+      <template #header>
+        <h1>login</h1>
+      </template>
+      <template #content>
+        <Form>
+            <label for="Email">Email</label>
+          <InputText
+          class="w-23rem"
+          id="Email"
+          placeholder="Digite o seu email"/>
+          <label for="Senha">Senha</label>
+          <InputText
+          class="w-23rem"
+          id="Senha"
+          placeholder="Digite a sua senha"/>
+          <Button
+          class="w-9rem my-3"
+          style="margin-left: 7rem;"
+          type="submit"
+          label="Entrar"/>
+        </Form>
+      </template>
+      <template #footer>
+        <p class="cursor-pointer" @click="router.push('/cadastro');">Não Tem uma Conta? Se cadastre!</p>
+      </template>
+    </Card>
   </div>
 </template>
 
