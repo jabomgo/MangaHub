883ad47c4a273a3378839cfa591797214f60bca9
cidy <cidywallace12@gmail.com>
2025-04-27 16:35:46 -0300

    adicionando função de criar e deletar favorito

--- src/axiosDB.js
+++ src/axiosDB.js
@@ -31,6 +31,31 @@ class axiosDB {
       console.error(error)
     }
   }
+
+  async postFavoritoManga(dados) {
+    try{
+      await this.apiJsonDb.post("/users",dados)
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  async DeletFavorito(email, mangaId) {
+    try{
+      const response =  await this.apiJsonDb.get(`/users?email=${email}&mangaId=${mangaId}`)
+      const data = response.data
+      if (data.length === 0){
+        console.error("favorito não encontrado")
+        return null
+      }
+      const idFav = data[1]
+      const id = idFav.id
+      console.log(id)
+      await this.apiJsonDb.delete(`/users/${id}`)
+    }catch(error) {
+      console.error(error)
+    }
+  }
 }
 
 export default new axiosDB();
