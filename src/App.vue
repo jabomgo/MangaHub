64b43d9d95698e6b4c7900ea42d0313db9688f92
jonathan.neves <jonathan.neves@dcx.ufpb.br>
2025-04-27 14:58:10 -0300

    Rodapé creditando a API do primevue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,44 @@
 <script setup>
-import 'primeicons/primeicons.css'
-import { RouterView } from 'vue-router'
-import router from './router'
-import { ref } from 'vue'
+import "primeicons/primeicons.css";
+import { RouterView } from "vue-router";
+import router from "./router";
+import { ref } from "vue";
 const items = ref([
   {
-    label: 'MangaHub',
-    icon: 'pi pi-home',
-    command: () => pushTo('home'),
+    label: "MangaHub",
+    icon: "pi pi-home",
+    command: () => pushTo("home"),
   },
   {
-    label: 'Favoritos',
-    icon: 'pi pi-heart',
-    command: () => pushTo('favoritos'),
+    label: "Favoritos",
+    icon: "pi pi-heart",
+    command: () => pushTo("favoritos"),
   },
   {
-    label: 'Pesquisar',
-    icon: 'pi pi-search',
-    command: () => pushTo('pesquisando'),
+    label: "Pesquisar",
+    icon: "pi pi-search",
+    command: () => pushTo("pesquisando"),
   },
   {
-    label: 'Conta',
-    icon: 'pi pi-user',
+    label: "Conta",
+    icon: "pi pi-user",
     items: [
       {
-        label: 'Login',
-        icon: 'pi pi-key',
-        command: () => pushTo('login'),
+        label: "Login",
+        icon: "pi pi-key",
+        command: () => pushTo("login"),
       },
       {
-        label: 'Sair',
-        icon: 'pi pi-sign-out',
-        command: () => console.log('logout'),
+        label: "Sair",
+        icon: "pi pi-sign-out",
+        command: () => console.log("logout"),
       },
     ],
   },
-])
+]);
 
 function pushTo(routerName) {
-  router.push({ name: routerName })
+  router.push({ name: routerName });
 }
 </script>
 
@@ -49,6 +49,14 @@ function pushTo(routerName) {
     </div>
   </nav>
   <RouterView />
+  <div>
+    <footer class="flex justify-content-center py-6">
+      <p>
+        Dados fornecidos pela
+        <a href="https://mangadex.org" target="_blank">MangaDex API</a>
+      </p>
+    </footer>
+  </div>
 </template>
 
 <style scoped></style>
