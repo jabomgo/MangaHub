3ccd6ee9930f3a1e98e73fe9b43a77e4ab10a18c
jonathan.neves <jonathan.neves@dcx.ufpb.br>
2025-04-27 17:47:49 -0300

    página de notfound

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,10 +1,19 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 </script>
 
 <template>
-  <div>
-    <p>NotFound View</p>
+  <div class="flex flex-column align-items-center py-6">
+    <h1>Página não encontrada</h1>
+    <Button
+      class="p-button p-component p-button-primary"
+      @click="router.push({name: 'home'})"
+    >
+      Voltar para o início
+    </Button>
   </div>
 </template>
 
